bd3d8d1168177cf1a1d26288479ece67dd1773d9
RootK1d <irgendjemand31@gmail.com>
2026-04-29 12:41:06 +0200

    v2.5.0
    Updated scrapeEvents.js to use WoWheads new events data JSON

--- utils/scrapeEvents.js
+++ utils/scrapeEvents.js
@@ -3,10 +3,6 @@
 const axios = require('axios');
 const cheerio = require("cheerio");
 
-// Import the utility functions
-
-let { getStringBetween } = require("./getStringBetween.js");
-
 /**
  * The core scraping function for the events list from WoWHead.
  *
@@ -15,30 +11,22 @@ let { getStringBetween } = require("./getStringBetween.js");
  * @return {Promise}                Returns an array of objects of all events (no descriptions - request flood protection)
  * 
  * @author RootK1d
- * @since  2.0.0
+ * @since  2.5.0
  * @type   {Function}
  */
 
-function scrapeEvents(locale) {
+async function scrapeEvents(locale) {
 
     // Scrape the events list from WoWHead
 
-    let result = axios.get(`https://www.wowhead.com/${locale}/events`).then(({ data }) => {
-
-        events = [];
+    let result = await axios.get(`https://www.wowhead.com/${locale}/events`).then(({ data }) => {
 
         const $ = cheerio.load(data);
-        let site = $.html();
 
-        // Extract the events data from the site using regex
+        // Extract the events data from the site using JSON
+        let events = $("script[id^='data.page.listPage.listviews']").html();
 
-        let eventsData = Array.from(("{" + getStringBetween(getStringBetween(site, "var myTabs", "myTabs.flush()"), "{\"", ");\nnew Listview({")).matchAll('{"category":\\d*,"categoryName":"(?:.*?)","duration0":\\d*,"duration1":\\d*,"endDate":(?:"\\d*-\\d*-\\d* \\d*:\\d*:\\d*"|null),"id":\\d*,"name":"(?:.*?)","startDate":(?:"\\d*-\\d*-\\d* \\d*:\\d*:\\d*"|null),"occurrences":(?:\\[(?:{"start":(?:".*?")),"end":(?:".*?")}\\]|\\[\\]),"filtertype":(?:\\W*\\d*),"rec":(?:\\W*\\d*),"popularity":\\d*}', 'gi'))
-
-        eventsData.forEach((event) => {
-            events.push(JSON.parse(event[0]));
-        });
-
-        return events;
+        return JSON.parse(events)[0].data
 
     })
 
